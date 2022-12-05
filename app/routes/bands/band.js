fc8ae12091833ae54b8bdf9a39e21dc5c7682e5e
Riad Abdallah <riad.abdallah@hotmail.com>
2022-12-05 16:29:18 +0100

    Use catalogue for querying bands

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,10 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default class BandsBandRoute extends Route {
+  @service catalog;
+
   model(params) {
     // This route was generated with a dynamic segment. Implement data loading
     // based on that dynamic segment here in the model hook.
-    let bands = this.modelFor('bands');
-    return bands.find((band) => band.id == params.id);
+    return this.catalog.find('band', (band) => band.id === params.id);
   }
 }
