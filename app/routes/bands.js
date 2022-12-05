4aa3a82fd9f0c2c7f5a721d53ae97befa61250b1
Riad Abdallah <riad.abdallah@hotmail.com>
2022-12-05 16:36:29 +0100

    Add songs to the catalogue

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -9,7 +9,7 @@ export class Band {
   constructor({ id, name, songs }) {
     this.id = id;
     this.name = name;
-    this.songs = songs;
+    this.songs = songs || [];
   }
 }
 
@@ -27,25 +27,21 @@ export default class BandsRoute extends Route {
   model() {
     let blackDog = new Song({
       title: 'Black Dog',
-      band: 'Led Zeppelin',
       rating: 3,
     });
 
     let yellowLedbetter = new Song({
       title: 'Yellow Ledbetter',
-      band: 'Pearl Jam',
       rating: 4,
     });
 
     let pretender = new Song({
       title: 'The Pretender',
-      band: 'Foo Fighters',
       rating: 2,
     });
 
     let daughter = new Song({
       title: 'Daughter',
-      band: 'Pearl Jam',
       rating: 5,
     });
 
@@ -66,6 +62,16 @@ export default class BandsRoute extends Route {
       name: 'Foo Fighters',
       songs: [pretender],
     });
+
+    blackDog.band = ledZeppelin;
+    yellowLedbetter.band = pearlJam;
+    daughter.band = pearlJam;
+    pretender.band = fooFighters;
+
+    this.catalog.add('song', blackDog);
+    this.catalog.add('song', yellowLedbetter);
+    this.catalog.add('song', daughter);
+    this.catalog.add('song', pretender);
 
     this.catalog.add('band', ledZeppelin);
     this.catalog.add('band', pearlJam);
