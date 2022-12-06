5f3bb83c1cae2cb83158f22a961a18030b3f37b9
Riad Abdallah <riad.abdallah@hotmail.com>
2022-12-06 17:28:36 +0100

    Refactor data fetching from the backend

--- app/services/catalog.js
+++ app/services/catalog.js
@@ -1,30 +1,84 @@
 import Service from '@ember/service';
+import Band from 'rawre/models/band';
+import Song from 'rawre/models/song';
 import { tracked } from 'tracked-built-ins';
 
+function extractRelationships(object) {
+  let relationships = {};
+  for (let relationshipName in object) {
+    relationships[relationshipName] = object[relationshipName].links.related;
+  }
+
+  return relationships;
+}
+
 export default class CatalogService extends Service {
-    storage = {};
+  storage = {};
 
-    constructor() {
-        super(...arguments);
-        this.storage.bands = tracked([]);
-        this.storage.songs = tracked([]);
+  constructor() {
+    super(...arguments);
+    this.storage.bands = tracked([]);
+    this.storage.songs = tracked([]);
+  }
+
+  async fetchAll(type) {
+    if (type === 'bands') {
+      let response = await fetch(
+        'http://json-api.rockandrollwithemberjs.com/bands'
+      );
+      let json = await response.json();
+      this.loadAll(json);
+      return this.bands;
+    } else if (type === 'songs') {
+      let response = await fetch(
+        'http://json-api.rockandrollwithemberjs.com/songs'
+      );
+      let json = await response.json();
+      this.loadAll(json);
+      return this.songs;
+    }
+  }
+
+  loadAll(json) {
+    let records = [];
+    for (let item of json.data) {
+      records.push(this._loadResources(item));
     }
 
-    add(type, record) {
-        let collection = type === 'band' ? this.storage.bands : this.storage.songs;
-        collection.push(record);
+    return records;
+  }
+
+  _loadResources(data) {
+    let record;
+    let { id, type, attributes, relationships } = data;
+    if (type === 'bands') {
+      let rels = extractRelationships(relationships);
+      record = new Band({ id, ...attributes }, rels);
+      this.add('band', record);
+    } else if (type === 'songs') {
+      let rels = extractRelationships(relationships);
+      record = new Song({ id, ...attributes }, rels);
+      this.add('song', record);
     }
 
-    get bands() {
-        return this.storage.bands;
-    }
+    return record;
+  }
 
-    get songs() {
-        return this.storage.songs;
-    }
+  add(type, record) {
+    let collection = type === 'band' ? this.storage.bands : this.storage.songs;
+    collection.push(record);
+  }
 
-    find(type, filterFn) {
-        let collection = type === 'band' ? this.bands : this.songs;
-        return collection.find(filterFn);
-    }
+  get bands() {
+    return this.storage.bands;
+  }
+
+  get songs() {
+    return this.storage.songs;
+  }
+
+  find(type, filterFn) {
+    let collection = type === 'band' ? this.bands : this.songs;
+    return collection.find(filterFn);
+  }
 }
